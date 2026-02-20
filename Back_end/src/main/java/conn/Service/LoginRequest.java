d535b19d5089032880e753695086783cdbe4d6a2
Messias Macedo de Souza <messiasmacedodesouza2007@gmail.com>
2026-02-20 13:57:49 -0300

    Delete Back_end directory

--- Back_end/src/main/java/conn/Service/LoginRequest.java
+++
@@ -1,6 +0,0 @@
-package conn.Service;
-
-public class LoginRequest {
-    public String email;
-    public String senha;
-}
--- Back_end/src/main/java/conn/SpringBoot.java
+++
@@ -1,1 +0,0 @@
-package conn;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;@SpringBootApplicationpublic class SpringBoot {    public static void main(String[] args) {        SpringApplication.run(SpringBoot.class, args);    }}
